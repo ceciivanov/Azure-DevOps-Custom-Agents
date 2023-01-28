f689809f15b47942557e740f3826681e97672e49
Ceci Ivanov <61785341+cceeci99@users.noreply.github.com>
2023-01-28 18:04:18 +0200

    Update install-agent.sh

--- scripts/install-agent.sh
+++ scripts/install-agent.sh
@@ -8,12 +8,6 @@ AzureDevOpsURL=$1
 AzureDevOpsPAT=$2
 AgentPoolName=$3
 
-# define required arguments for the config
-# AzureDevOpsPAT=fnnzkwfgwqvdvejobyzuxrmu7rvotu6krrtl5q7ge3426caugaqq
-# AzureDevOpsURL=https://dev.azure.com/CIvanov0344
-# AgentPoolName=customAgents
-# VMadmin=agentadmin
-
 # Creates directory & download ADO agent install files
 mkdir myagent && cd myagent
 
